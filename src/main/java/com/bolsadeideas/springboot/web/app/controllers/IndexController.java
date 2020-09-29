4d0075670a2e143c98bf1159224a08e792291980
Yeison Vanegas <yeison.oyola@avaldigitallabs.com>
2020-09-28 20:27:31 -0500

    Anotación ModelAttribute

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -35,13 +36,18 @@ public class IndexController {
 	
 	@RequestMapping("/listUsers")
 	public String listar(Model model) {
+		
+		model.addAttribute("titulo", "Listado de Usuarios");
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres","Guzman","andres@correo.com"),
 				new Usuario("Paola","Perez","perez@correo.com"),
 				new Usuario("Camila","Ortiz","camila@correo.com"),
 				new Usuario("Pedro","Velez","pedro@correo.com"));
-		model.addAttribute("titulo", "Listado de Usuarios");
-		model.addAttribute("usuarios", usuarios);
-		return "listar";
+		return usuarios;
 	}
 
 }
